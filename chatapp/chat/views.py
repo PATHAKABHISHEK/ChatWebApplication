2b3f5a9ec25d85d120c1294e7298c6dc5ab0ae5a
Abhishek Pathak <pathakabhishek0003@gmail.com>
2019-05-16 12:58:52 +0530

    added register page

--- chatapp/chat/views.py
+++ chatapp/chat/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def register_login(request):
+    return render(request=request,template_name='chat/register_login.html',
+                    context={'title':'ChatApp'})
