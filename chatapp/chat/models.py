2d911adadf5a075351063eb38c763913c570e6a1
Abhishek Pathak <pathakabhishek0003@gmail.com>
2019-05-16 13:37:29 +0530

    created user model

--- chatapp/chat/models.py
+++ chatapp/chat/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    first_name = models.CharField(max_length=20)
+    last_name  = models.CharField(max_length=20)
+    user_name  = models.CharField(max_length=50)
+    email_id   = models.CharField(max_length=50)
+    password   = models.CharField(max_length=50)
